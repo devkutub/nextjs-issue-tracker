7df9fb29c5cf1918ede88a9e3363549fc7f33f03
Kutub <kutubuddin2528@gmail.com>
2023-10-19 18:49:43 +0530

    implement client side error validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,29 @@
 'use client'
 import React from 'react'
-import { TextField, TextArea, Button, Callout } from "@radix-ui/themes"
+import { TextField, TextArea, Button, Callout, Text } from "@radix-ui/themes"
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { AiFillWarning } from "react-icons/ai"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from '@/app/ValidationSchemas';
+import { z } from "zod";
 
-interface NewIssue {
-    title: string;
-    description: string;
-}
+// interface NewIssue {
+//     title: string;
+//     description: string;
+// }
+
+type NewIssue = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
     const [error, setError] = React.useState('');
     const router = useRouter()
-    const { register, control, handleSubmit } = useForm<NewIssue>();
+    const { register, control, handleSubmit, formState: { errors } } = useForm<NewIssue>({
+        resolver: zodResolver(createIssueSchema)
+    });
 
     return (
         <div className='max-w-xl'>
@@ -41,11 +48,13 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder='Title' {...register("title")} />
                 </TextField.Root>
+                {errors.title && <Text color='red' as="p">{errors.title.message}</Text>}
                 <Controller
                     name='description'
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
                 />
+                {errors.description && <Text color='red' as="p">{errors.description.message}</Text>}
                 {/* <SimpleMDE placeholder='Description' /> */}
                 {/* <TextArea placeholder='Description' /> */}
                 <Button>Submit New Issue</Button>
