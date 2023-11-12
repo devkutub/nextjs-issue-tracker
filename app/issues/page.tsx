2a924e701213f89e064abb850475b2b212b01e97
Kutub <kutubuddin2528@gmail.com>
2023-11-12 11:41:17 +0530

    rename issue status badge file

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Table } from "@radix-ui/themes"
 import Link from 'next/link'
 import prisma from '@/prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBardge'
+import IssueStatusBadge from '../components/IssueStatusBadge'
 
 const page = async () => {
     const issues = await prisma.issue.findMany();
